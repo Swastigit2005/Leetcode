99e059ad4319e41b17aa85a5ac63bab59ab3436c
Swasti Jain <swastiworkspace@gmail.com>
2025-09-08 23:46:16 +0530

    Time: 23 ms (64.81%) | Memory: 69 MB (55.34%) - LeetSync

--- 817-design-hashmap/design-hashmap.cpp
+++ 817-design-hashmap/design-hashmap.cpp
@@ -1,24 +1,38 @@
 class MyHashMap {
 public:
-    int data[1000001];
+    int n = 10000;
+    vector<vector<pair<int,int>>> arr;
+
     MyHashMap() {
-        fill(data, data + 1000001, -1);
+        arr.resize(n);  
     }
-    void put(int key, int val) {
-        data[key] = val;
+
+    void put(int key, int value) {
+        int idx = key % n;
+        for (auto &[a, b] : arr[idx]) {
+            if (a == key) {
+                b = value;
+                return;
+            }
+        }
+        arr[idx].push_back({key, value});
     }
+
     int get(int key) {
-        return data[key];
+        int idx = key % n;
+        for (auto &[a, b] : arr[idx]) {
+            if (a == key) return b;
+        }
+        return -1;
     }
+
     void remove(int key) {
-        data[key] = -1;
+        int idx = key % n;
+        for (int i = 0; i < arr[idx].size(); i++) {
+            if (arr[idx][i].first == key) {
+                arr[idx].erase(arr[idx].begin() + i);
+                return;
+            }
+        }
     }
 };
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
